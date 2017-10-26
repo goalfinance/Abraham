dcb82e530b46117f94bdfb84439e809d0bf553a9
panqingrong <panpanqingrong@icloud.com>
2017-10-26 17:47:49 +0800

    1. Add the module 'gabriel' as a eureca server. 2. Add the module 'isaac' as a File-manager restful server with reactive architecture. (Spring webflux + MongoDB) 3. Enhance the module 'Abraham-web' as a eureca discovery client.

---
+++ gabriel/src/main/java/faith/gabriel/GabrielApplication.java
@@ -0,0 +1,14 @@
+package faith.gabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class GabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielApplication.class, args);
+	}
+}
